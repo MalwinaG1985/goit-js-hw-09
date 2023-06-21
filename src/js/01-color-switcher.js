5e378e6ef8d834b06cd708b81aa8035aaff147fb
Malwina Grysiak <malwinagrysiak@gmail.com>
2023-06-21 20:43:16 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyColor = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalID = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.setAttribute('disabled', '');
+
+startBtn.addEventListener('click', e => {
+    e.target.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+
+    intervalID = setInterval(() => {
+        bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', e => {
+    e.target.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+    stopBtn.disabled = false;
+
+    clearInterval(intervalID);
+});
